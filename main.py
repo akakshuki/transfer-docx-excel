3a98733dbc591cd4b89da41576a535883ba765fe
Hoang Le <le.dinhhoang.1207@outlook.com>
2023-02-19 22:42:13 +0700

    optimize code

--- main.py
+++ main.py
@@ -27,10 +27,8 @@ LEVEL_MAP = {
 def convert_file_word_to_excel(input_file_path, output_file_path):
     if input_file_path == "":
         sys.exit(0)
-
     # Read the contents of the Microsoft Word file
     doc = docx.Document(input_file_path)
-
     # Store the contents of the file in a list
     data = []
     questions = []
@@ -49,7 +47,6 @@ def convert_file_word_to_excel(input_file_path, output_file_path):
             option_d = data[i+4]
             keyword = re.findall(r'\((.*?)\)', element)[0]
             level = LEVEL_MAP.get(keyword, 'Vận dụng cao')
-
             questions_for_qc.append(
                 Question(statement, level, option_a, option_b, option_c, option_d)
             )
@@ -63,9 +60,7 @@ def convert_file_word_to_excel(input_file_path, output_file_path):
             option_c = data[i+3].replace("C.", "")
             option_d = data[i+4].replace("D.", "")
             keyword = re.findall(r'\((.*?)\)', element)[0]
-
             level = LEVEL_MAP.get(keyword, 'Vận dụng cao')
-
             questions.append(Question(statement, level, option_a, option_b, option_c, option_d))
 
     # Convert data list to pandas data frame
@@ -75,46 +70,43 @@ def convert_file_word_to_excel(input_file_path, output_file_path):
         row = [question.statement, question.level, question.option_a,
                question.option_b, question.option_c, question.option_d]
         rows.append(row)
-    df = pd.DataFrame(rows, columns)
+    df_final = pd.DataFrame(rows, columns)
 
     rows_for_qc = []
     for question in questions_for_qc:
         row = [question.statement, question.level, question.option_a,
                question.option_b, question.option_c, question.option_d]
         rows_for_qc.append(row)
-
     df_qc = pd.DataFrame(rows_for_qc, columns)
-
 
     # Create an Excel Workbook and add the DataFrame as a worksheet
     book = Workbook()
     writer = pd.ExcelWriter(
         './output/output_data.xlsx' if output_file_path == "" else output_file_path, engine='openpyxl')
     writer.book = book
-    df.to_excel(writer, index=False, header=False, sheet_name='questions')
+    df_final.to_excel(writer, index=False, header=False, sheet_name='questions')
     df_qc.to_excel(writer, index=False, header=False, sheet_name='questi')
     # Save the Excel Workbook
     writer.save()
 
 # Remove empty file input
 # ==========================
+ pylint --fail-under=8 python_code.py
 
 
-def conver_txt_file(input, output):
+def conver_txt_file(input_file_path, output_file_path):
     # Open the input file
-    if input == "":
+    if input_file_path == "":
         sys.exit(0)
     with open(input, "r", encoding="utf-8") as input_file:
         # Read the contents of the file
         lines = input_file.readlines()
-
     # Remove empty lines from the list of lines
     lines = list(filter(lambda x: x.strip() != "", lines))
 
     # Open the output file and write the filtered lines to it
-    with open('./output/output_data.txt' if output == "" else output, "w", encoding='utf-8') as output_file:
+    with open('./output/output_data.txt' if output_file_path == "" else output_file_path, "w", encoding='utf-8') as output_file:
         output_file.writelines(lines)
-
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
@@ -125,7 +117,6 @@ if __name__ == '__main__':
     parser.add_argument('output_address', nargs='?',
                         default='', help='output file address (optional)')
     args = parser.parse_args()
-
     if args.function_name == 'cv':
         convert_file_word_to_excel(args.input_address, args.output_address)
     elif args.function_name == 'rm':
