df9d4dcba5cd2abda33738183c00360c5f03953d
Hoang D. Le <45985646+akakshuki@users.noreply.github.com>
2023-03-04 17:13:46 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import argparse
 import re
 import sys
-
 import pandas as pd
 from openpyxl import Workbook
 
@@ -29,7 +28,7 @@ LEVEL_MAP = {
 drive.mount('/content/drive/')
 
 def convert_file_word_to_excel(input_file_path, output_file_path):
-    if input_file_path == "":
+    if not input_file_path:
         sys.exit(0)
     # Store the contents of the file in a list
     data = []
@@ -79,37 +78,4 @@ def convert_file_word_to_excel(input_file_path, output_file_path):
     rows_for_qc = []
     for question in questions_for_qc:
         row = [question.statement, question.level, question.option_a,
-               question.option_b, question.option_c, question.option_d, question.answer]
-        rows_for_qc.append(row)
-    df_qc = pd.DataFrame(rows_for_qc, columns=columns)
-    # Create an Excel Workbook and add the DataFrame as a worksheet
-    book = Workbook()
-    writer = pd.ExcelWriter(
-        './output/output_data.xlsx' if output_file_path == "" else output_file_path, engine='openpyxl')
-    # Remove default sheet
-    del book['Sheet']
-    writer.book = book
-    df_final.to_excel(writer, index=False, header=False,
-                      sheet_name='questions')
-    df_qc.to_excel(writer, index=False, header=False,
-                   sheet_name='question_for_qc')
-    # Save the Excel Workbook
-    writer.save()
-
-# Remove empty file input
-# ==========================
-
-
-def conver_txt_file(input_file_path, output_file_path):
-    # Open the input file
-    if input_file_path == "":
-        sys.exit(0)
-    with open(file=input_file_path, mode="r", encoding="utf-8") as input_file:
-        # Read the contents of the file
-        lines = input_file.readlines()
-    # Remove empty lines from the list of lines
-    lines = list(filter(lambda x: x.strip() != "", lines))
-
-    # Open the output file and write the filtered lines to it
-    with open(file='./output/output_data.txt' if output_file_path == "" else output_file_path, mode="w", encoding='utf-8') as output_file:
-        output_file.writelines(lines)
+               question.option_b, question.option_c,
